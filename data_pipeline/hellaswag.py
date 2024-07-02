0d4197ab31f95cf0f65d96107a60f64f7b1c7b47
Yangjianxiao0203 <jasperyang0203@gmail.com>
2024-07-02 22:45:51 +0800

    qwen 0.5B sft

--- data_pipeline/hellaswag.py
+++ data_pipeline/hellaswag.py
@@ -4,7 +4,11 @@ from transformers import AutoTokenizer
 
 accelerator = Accelerator()
 
-def process(single_data,tokenizer,max_length=128):
+'''
+TODO：hellaswag 评测数据集都是怎么构建的，具体如何评估模型能力，是看困惑度还是准确度, 是训练中使用，还是训练后单独评估
+'''
+
+def process(single_data, tokenizer, max_length=128):
     '''
     数据处理流程：
     1. 把prompt（source） 和 输出 output（target）拼接在一起，作为同一个input ids
@@ -18,9 +22,9 @@ def process(single_data,tokenizer,max_length=128):
 
     ### Instruction:
     {instruction}
-    
+
     {input_formatter}
-    
+
     ### Response:
     """
     input_formatter = ""
@@ -31,7 +35,7 @@ def process(single_data,tokenizer,max_length=128):
     tokenized_prompt = tokenizer(prompt, add_special_tokens=False)
     tokenized_response = tokenizer(single_data["output"], add_special_tokens=False)
 
-    #注意： attention mask代表需要注意力机制看到的部分，所以eos token也是需要关注的，所以attention mask补充为1
+    # 注意： attention mask代表需要注意力机制看到的部分，所以eos token也是需要关注的，所以attention mask补充为1
     input_ids = tokenized_prompt["input_ids"] + tokenized_response["input_ids"] + [tokenizer.pad_token_id]
     attention_mask = tokenized_prompt["attention_mask"] + tokenized_response["attention_mask"] + [1]
     labels = [-100] * len(tokenized_prompt["input_ids"]) + tokenized_response["input_ids"] + [tokenizer.pad_token_id]
@@ -56,21 +60,18 @@ def process(single_data,tokenizer,max_length=128):
         "labels": labels
     }
 
-def build_alpaca_data(tokenizer, data_dir = "tatsu-lab/alpaca", size = None,max_length=128):
+def build_hellaswag_data(tokenizer, data_dir = "Rowan/hellaswag", size = None,max_length=128):
     dataset = load_dataset(data_dir)
-    # tokenizer = AutoTokenizer.from_pretrained("C:\jianxiao_codes\python\models\qwen/Qwen2-0.5B\qwen\Qwen2-0___5B")
-
-    # dataset = dataset["train"].select(range(10000))
     if size:
-        dataset = dataset["train"].select(range(size))
+        dataset = dataset["val"].select(range(size))
     else:
-        dataset = dataset["train"]
+        dataset = dataset["val"]
     processed_dataset = dataset.map(lambda x: process(x, tokenizer,max_length=max_length), batched=False,remove_columns=dataset.column_names)
     return processed_dataset
 
 if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained("C:\jianxiao_codes\python\models\qwen/Qwen2-0.5B\qwen\Qwen2-0___5B")
-    dataset = build_alpaca_data(tokenizer)
+    dataset = build_hellaswag_data(tokenizer)
     for i in dataset:
         print(i)
         break
---
+++ trainning_pipeline/train_v2.py
@@ -0,0 +1,76 @@
+from evaluate import load
+import numpy as np
+import torch
+from datasets import load_metric
+from transformers import Trainer, AutoModelForCausalLM, AutoTokenizer, TrainingArguments
+from peft import LoraConfig, TaskType, get_peft_model
+from data_pipeline.alpaca import build_alpaca_data
+from accelerate import Accelerator
+import wandb
+
+
+wandb.login(key="d7ff37534eeca77d459824f5c4be2958e279ec07")
+
+# Initialize wandb project
+wandb.init(project="qwen_0.5b_sft")
+#wandb.init(project="qwen_0.5b_sft",mode="offline")
+
+# 困惑度计算函数
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    if isinstance(logits, tuple):
+        logits = logits[0]
+
+    valid_indices = labels != -100
+    predictions = np.argmax(logits, axis=-1)
+    predictions = predictions[valid_indices].flatten()
+    references = labels[valid_indices].flatten()
+
+    # 加载准确率度量
+    accuracy_metric = load("accuracy")
+
+    accuracy = accuracy_metric.compute(predictions=predictions, references=references)
+
+    # return {"perplexity": perplexity, "accuracy": accuracy["accuracy"]}
+    return {"accuracy": accuracy["accuracy"]}
+
+def train(model_path):
+    model =AutoModelForCausalLM.from_pretrained(model_path, torch_dtype="auto",device_map="auto")
+
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    dataset = build_alpaca_data(tokenizer,size=12)
+
+    training_args = TrainingArguments(
+        output_dir="./results",
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        learning_rate=5e-5,
+        per_device_train_batch_size=6,
+        per_device_eval_batch_size=6,
+        num_train_epochs=3,
+        weight_decay=0.01,
+        push_to_hub=False,
+        logging_dir="./logs",
+        report_to="wandb"
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset,
+        eval_dataset=dataset,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics,
+    )
+
+    trainer.train()
+
+    # Save the model if needed
+    trainer.save_model("output_model_path")
+    tokenizer.save_pretrained("output_model_path")
+
+
+if __name__ == "__main__":
+    train("C:\jianxiao_codes\python\models\qwen/Qwen2-0.5B\qwen\Qwen2-0___5B")
+
+
